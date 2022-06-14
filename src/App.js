98ddcf635ba5686b9783bee6d437e6e6de10bbee
Yu Shi <yushi@yus-mac-mini.mynetworksettings.com>
2022-06-14 00:23:15 -0400

    working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='text-black'>
+      <h1 className='w-full text-3xl font-bold'>Yu Shi</h1>
+      <ul className='flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Skills</li>
+        <li className='p-4'>Projects</li>
+        <li className='p-4'>Blogs</li>
+        <li className='p-4'>Resume</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+    </div>
   )
 }
 
