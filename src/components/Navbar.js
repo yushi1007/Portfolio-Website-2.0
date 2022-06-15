705c2eef660c938bfd3bcd1122535fbc54163106
Yu Shi <yushi@yus-mac-mini.mynetworksettings.com>
2022-06-14 22:15:21 -0400

    working on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='text-black'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
       <h1 className='w-full text-3xl font-bold'>Yu Shi</h1>
       <ul className='flex'>
         <li className='p-4'>Home</li>
