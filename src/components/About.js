f2983ac474c73bdc6b6e989520259e02cb0caa74
Yu Shi <yushi@yus-mac-mini.mynetworksettings.com>
2022-09-04 23:10:15 -0400

    working on about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,25 +6,27 @@ const About = () => {
   return (
     <div id='about' className='text-white mt-40 flex justify-between items-center mx-auto px-6 md:px-6 lg:px-6 w-full'>
       <div className='container mx-auto px-6'>
-        <h2 class="text-3xl before:block before:mb-1 before:rounded-full before:h-2 before:w-16 before:-translate-y-2 before:bg-indigo-600 before:content-[''] mb-20">About Me</h2>
-        <div className='flex flex-col xl:flex-row gap-24'>
-          <div>
-            <img src={Image} alt=''/>  
+        <header className='flex flex-col items-center text-center'>
+          <h2 className="text-4xl mb-20 text-transform: uppercase after:mx-auto after:mb-0 after:block after:mt-9 after:rounded-full after:h-1 after:w-24 after:-translate-y-1 after:bg-indigo-600 after:content-['']">About Me</h2>
+        </header>
+        <div className='flex flex-wrap items-center'>
+          <div className='w-full sm:w-2/5 flex items-center justify-center'>
+            <div className='w-2/3 flex justify-center'>
+              <div>
+                <img src={Image} alt='profile img'/>
+              </div>
+            </div>
           </div>
-          <div className='flex felx-col items-center lg:items-start text-center md:text-center lg:text-left'>
-            <div className='flex flex-col'>
-              <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3'>Yu Shi</h2>
-              <p className="mb-4 text-indigo-400 after:block after:mb-8 after:rounded-full after:h-1 after:w-16 after:translate-y-3 after:bg-indigo-600 after:content-['']">Web Developer</p>
-              <ul className='space-y-8'>
+          <div className='w-full sm:w-3/5 order-last'>
+            <ul className='space-y-8'>
               {aboutMe.map((item,index) => {
                 return(
                   <li key={index}>
-                    <p>{item.description}</p>
+                    <p className='text-gray-400 leading-relaxed mb-3'>{item.description}</p>
                   </li>            
                 )
               })}
-              </ul>
-            </div>
+            </ul>
           </div>
         </div>
       </div>
