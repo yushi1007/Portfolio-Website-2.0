6a1ab31119f3e59f74e3428339ab75e09ba78a9c
Yu Shi <yushi@macbook-pro.mynetworksettings.com>
2022-10-26 21:37:50 -0400

    add loading page

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/img/logo.png";
 
 const Loader = () => {
   return (
-    <div>
-        Loading
+    <div className="w-full h-[100vh] max-w-full m-0 flex justify-center items-center text-center overflow-hidden bg-primary">
+      <div
+        id="loading"
+        className="w-full h-[100vh] max-w-full m-0 flex justify-center items-center text-center overflow-hidden"
+      >
+        <img src={Logo} alt="logo" className="w-[100px] h-[100px]" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
